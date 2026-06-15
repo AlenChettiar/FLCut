c7b0d1f70ab293c7b6f2a638d8b55460f47c0002
Alen Chettiar <alen.demello1981@gmail.com>
2026-06-15 22:40:49 +0530

    fix: login at producition

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,6 @@
 import { PrismaClient } from "@prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
-
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    adapter: new PrismaPg({
-      connectionString: process.env.DATABASE_URL!,
-    }),
-  })
+  return new PrismaClient()
 }
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
